3423877b1d1473e8647d4394afd1ea088d5632be
saha-sagnik <sagnik12347@gmail.com>
2024-08-07 01:18:38 +0530

    recommendation system started

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -1,15 +1,20 @@
 import { Wrapper } from "./wrapper";
 import Toggle from "./toggle";
+import Recommended from "./Recommended";
+import { getRecommended } from "@/lib/recommended-service";
 
-const Sidebar = () => {
+const Sidebar = async () => {
 
     //fetch followers
     //fetch recommendations
-
+   const recommended = await getRecommended();
 
     return ( 
         <Wrapper>
            <Toggle/>
+           <div className="space-y-4 pt-4 lg:pt-0">
+               <Recommended data={recommended}/>
+           </div>
         </Wrapper>
      );
 }
